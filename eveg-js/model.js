f49604c6ea0b2fbc6bc05fd0efee128c99362f34
Jack Farmer <jack.farmer@warwick.ac.uk>
2021-02-25 15:21:33 +0000

    got all the veg pages working, will now do the basket

--- eveg-js/model.js
+++ eveg-js/model.js
@@ -4,73 +4,81 @@ function getProductDetails() {
   productDetails["carrots"]["images"] = ["img/carrots/carrot1.jpg", "img/carrots/carrot2.jpg", "img/carrots/carrot3.jpg", "img/carrots/carrot4.jpg", "img/carrots/carrot5.jpeg"];
   productDetails["carrots"]["page"] = "carrots.html";
   productDetails["carrots"]["name"] = "Carrots";
-  productDetails["carrots"]["description"] = "not just for rabbits";
+  productDetails["carrots"]["description"] = "The carrot is a root vegetable, usually orange in color, though purple, black, red, white, and yellow cultivars exist. They are a domesticated form of the wild carrot, Daucus carota, native to Europe and Southwestern Asia. The plant probably originated in Persia and was originally cultivated for its leaves and seeds. The most commonly eaten part of the plant is the taproot, although the stems and leaves are also eaten. The domestic carrot has been selectively bred for its greatly enlarged, more palatable, less woody-textured taproot.";
   productDetails["carrots"]["units"] = "kg";
   productDetails["carrots"]["price"] = 4.95;
   productDetails["carrots"]["pricePerUnit"] = 0.99;
+  productDetails["carrots"]["page"] = "carrots.html";
 
   productDetails["bananas"] = {};
   productDetails["bananas"]["images"] = ["img/bananas/banana1.jpg", "img/bananas/banana2.jpg", "img/bananas/banana3.jpg", "img/bananas/banana4.jpg", "img/bananas/banana5.jpg"];
   productDetails["bananas"]["page"] = "bananas.html";
   productDetails["bananas"]["name"] = "Bananas";
-  productDetails["bananas"]["description"] = "Yellow and banana-shaped";
+  productDetails["bananas"]["description"] = "A banana is an elongated, edible fruit (botanically a berry) produced by several kinds of large herbaceous flowering plants in the genus Musa. In some countries, bananas used for cooking may be called \"plantains\", distinguishing them from dessert bananas. The fruit is variable in size, color, and firmness, but is usually elongated and curved, with soft flesh rich in starch covered with a rind, which may be green, yellow, red, purple, or brown when ripe. The fruits grow in clusters hanging from the top of the plant.";
   productDetails["bananas"]["units"] = "500g";
   productDetails["bananas"]["price"] = 6.98;
   productDetails["bananas"]["pricePerUnit"] = 1.29;
+  productDetails["bananas"]["page"] = "bananas.html";
 
   productDetails["coconut"] = {};
   productDetails["coconut"]["images"] = ["img/coconuts/coconut1.jpg", "img/coconuts/coconut2.jpg", "img/coconuts/coconut3.jpg", "img/coconuts/coconut4.jpg", "img/coconuts/coconut5.jpg"];
   productDetails["coconut"]["page"] = "coconut.html";
   productDetails["coconut"]["name"] = "Coconut";
-  productDetails["coconut"]["description"] = "That exotic stuff";
+  productDetails["coconut"]["description"] = "The coconut is a round and elongate fruit of white, fibrous and oily meat covered by a thick, brown and hairy shell. Coconuts are surrounded by a fibrous kernel, inside which is a white meat called copra. When the fruit is still tender it yields a milky fluid that is commonly used as basic food in some zones. There is a predominant use of coconut in the production of copra: the dried kernel of the coconut, from which oil is obtained.";
   productDetails["coconut"]["units"] = "unit";
   productDetails["coconut"]["price"] = 5.98;
   productDetails["coconut"]["pricePerUnit"] = 2.99;
+  productDetails["coconut"]["page"] = "coconut.html";
 
   productDetails["apples"] = {};
   productDetails["apples"]["images"] = ["img/apples/apple1.jpg", "img/apples/apple2.jpg", "img/apples/apple3.jpg", "img/apples/apple4.jpg", "img/apples/apple5.jpg"];
   productDetails["apples"]["page"] = "apples.html";
   productDetails["apples"]["name"] = "Apples";
-  productDetails["apples"]["description"] = "Red, sweet and juicy";
+  productDetails["apples"]["description"] = "An apple is an edible fruit produced by an apple tree. Apple trees are cultivated worldwide and are the most widely grown species in the genus Malus. The tree originated in Central Asia, where its wild ancestor is still found today. Apples have been grown for thousands of years in Asia and Europe and were brought to North America by European colonists. Apples have religious and mythological significance in many cultures, including Norse, Greek, and European Christian tradition.";
   productDetails["apples"]["units"] = "kg";
   productDetails["apples"]["price"] = 3.99;
   productDetails["apples"]["pricePerUnit"] = 1.49;
+  productDetails["apples"]["page"] = "apples.html";
 
   productDetails["cherries"] = {};
   productDetails["cherries"]["images"] = ["img/cherries/cherry1.jpg", "img/cherries/cherry2.jpg", "img/cherries/cherry3.jpg", "img/cherries/cherry4.jpg", "img/cherries/cherry5.jpg"];
   productDetails["cherries"]["page"] = "cherries.html";
   productDetails["cherries"]["name"] = "Cherries";
-  productDetails["cherries"]["description"] = "Cherry pancake anyone?";
+  productDetails["cherries"]["description"] = "Cherries are a fleshy drupe (stone fruit) that are generally heart-shaped to nearly globular, about 2 cm (1 inch) in diameter, and varying in colour from yellow through red to nearly black. The acid content of the sweet cherry is low. The higher acid content of the sour cherry produces its characteristic tart flavour.";
   productDetails["cherries"]["units"] = "500g";
   productDetails["cherries"]["price"] = 0.99;
   productDetails["cherries"]["pricePerUnit"] = 1.99;
+  productDetails["cherries"]["page"] = "cherries.html";
 
   productDetails["tomatoes"] = {};
   productDetails["tomatoes"]["images"] = ["img/tomatoes/tomato1.jpg", "img/tomatoes/tomato2.jpg", "img/tomatoes/tomato3.jpg", "img/tomatoes/tomato4.jpg", "img/tomatoes/tomato5.jpg"];
   productDetails["tomatoes"]["page"] = "tomatoes.html";
   productDetails["tomatoes"]["name"] = "Tomatoes";
-  productDetails["tomatoes"]["description"] = "Red and ripe";
+  productDetails["tomatoes"]["description"] = "The tomato is the edible berry of the tomato plant. The species originated in western South America and Central America. The Nahuatl (the language used by the Aztecs) word tomatl gave rise to the Spanish word tomate, from which the English word tomato derived. Its domestication and use as a cultivated food may have originated with the indigenous peoples of Mexico.";
   productDetails["tomatoes"]["units"] = "500g";
   productDetails["tomatoes"]["price"] = 16.99;
   productDetails["tomatoes"]["pricePerUnit"] = 1.99;
+  productDetails["tomatoes"]["page"] = "tomatoes.html";
 
   productDetails["potatoes"] = {};
   productDetails["potatoes"]["images"] = ["img/potatoes/potato1.jpg", "img/potatoes/potato2.jpg", "img/potatoes/potato3.jpg", "img/potatoes/potato4.jpg", "img/potatoes/potato5.jpg"];
   productDetails["potatoes"]["page"] = "potatoes.html";
   productDetails["potatoes"]["name"] = "Potatoes";
-  productDetails["potatoes"]["description"] = "Boil them, bake them, mash them...";
+  productDetails["potatoes"]["description"] = "The potato is native to the Peruvian-Bolivian Andes and is one of the world’s main food crops. Potatoes are frequently served whole or mashed as a cooked vegetable and are also ground into potato flour, used in baking and as a thickener for sauces. The tubers are highly digestible and supply vitamin C, protein, thiamin, and niacin.";
   productDetails["potatoes"]["units"] = "kg";
   productDetails["potatoes"]["price"] = 8.61;
   productDetails["potatoes"]["pricePerUnit"] = 0.99;
+  productDetails["potatoes"]["page"] = "potatoes.html";
 
   productDetails["beans"] = {};
   productDetails["beans"]["images"] = ["img/beans/bean1.jpg", "img/beans/bean2.jpg", "img/beans/bean3.jpg", "img/beans/bean4.jpg", "img/beans/bean5.jpg"];
   productDetails["beans"]["page"] = "beans.html";
   productDetails["beans"]["name"] = "Beans";
-  productDetails["beans"]["description"] = "Green and healthy";
+  productDetails["beans"]["description"] = "Green beans are the unripe, young fruit of various cultivars of the common bean. Immature or young pods of the runner bean, yardlong bean, and hyacinth bean are used in a similar way. Green beans are known by many common names, including French beans, string beans, snap beans, snaps, haricot vert, or just plain 'beans'";
   productDetails["beans"]["units"] = "kg";
   productDetails["beans"]["price"] = 41.01;
   productDetails["beans"]["pricePerUnit"] = 1.29;
+  productDetails["beans"]["page"] = "beans.html";
 
   return productDetails;
 }
@@ -121,6 +129,7 @@ function calculateTotals() {
 }
 
 function addToBasket(product, quantity) {
+
   if (document.cookie.indexOf(product) == -1) {
     createEmptyBasket();
   }

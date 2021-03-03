8cd1d10844d9a42985b39399fb3384e3d1e479bc
Jack Farmer <jack.farmer@warwick.ac.uk>
2021-03-03 00:21:18 +0000

    first draft done, need to add donloadable invoice and change some fonts but main stuff done

--- eveg-js/model.js
+++ eveg-js/model.js
@@ -120,6 +120,17 @@ function readBasket() {
   return basket;
 }
 
+function clearBasket() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+
 function calculateTotals() {
   var basket = readBasket();
   var productDetails = getProductDetails();
